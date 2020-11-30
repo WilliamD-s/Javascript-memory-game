36f74f99566ae344f390e7feb1d866c7e42cb07a
WilliamD-s <William231297@outlook.com>
2020-11-29 23:33:33 -0300

    including the done project.

---
+++ jogo_memoria/index.js
@@ -0,0 +1,80 @@
+const cardBoard = document.querySelector("#cardboard");
+const images = [
+    'card_1.jpg',
+    'card_2.jpg',
+    'card_3.jpg',
+    'card_4.jpg',
+    'card_5.jpg',
+    'card_6.jpg'
+];
+
+let cardHTML = "";
+
+images.forEach(img => {
+    cardHTML += `
+    <div class="memory-card" data-card="${img}">
+        <img class="front-face" src="img/${img}">
+        <img class="back-face" src="img/verso.jpg">
+    </div>
+    `
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/* FIM RENDERIZAÇÃO HTML */
+
+const cards = document.querySelectorAll(".memory-card");
+
+let firstCard, secondCard, controle = 0;
+let lockCard = false;
+
+function flipCard(){
+    if(lockCard) return false;
+    this.classList.add('flip');
+    if(!firstCard){
+        firstCard = this;
+        return false;
+    }
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+    !isMatch?disableCards():resetCards(isMatch);
+}
+
+function disableCards(){
+    lockCard = true;
+    setTimeout(()=>{ 
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetCards();
+    },1000);
+}
+
+(function shuffle(){
+    cards.forEach( card => {
+        let rand = Math.floor(Math.random() * 12);
+        card.style.order = rand;
+    });
+})();
+
+function resetCards(isMatch = false){
+    if(isMatch){
+        firstCard.removeEventListener('click',flipCard);
+        secondCard.removeEventListener('click',flipCard);        
+        winner();
+    }
+    [firstCard, secondCard, lockCard] = [null, null, false];
+}
+
+function winner(){
+    controle += 1;
+    if(controle>5){
+        alert("Ganhou");
+    }   
+}
+
+cards.forEach(card => card.addEventListener('click',
+flipCard));
